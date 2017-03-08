6e4a4404df20b7d33fc6a34a927412ae131f596f
chasel <1620646939@qq.com>
2017-03-08 14:38:22 +0800

    暂时没什么用的类

--- src/com/chasel/main/MainClass.java
+++ src/com/chasel/main/MainClass.java
@@ -1,5 +1,28 @@
 package com.chasel.main;
 
+import com.chasel.init.InitContainer;
+import com.chasel.service.UserServiceImpl;
+
 public class MainClass {
 
+	/**
+	 * 不使用启动类解析，直接new
+	 * 必然会抛空指针异常
+	 * @param args
+	 */
+//	public static void main(String[] args) {
+//		UserServiceImpl user = new UserServiceImpl();
+//		user.getData();
+//	}
+	
+	/**
+	 * 启动容器来获取类对象
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		InitContainer contain = new InitContainer();
+//		System.out.println(contain.getClass().isInterface());
+//		UserServiceImpl user = (UserServiceImpl) contain.getBean("com.chasel.service.UserServiceImpl");
+//		user.getData();
+	}
 }
