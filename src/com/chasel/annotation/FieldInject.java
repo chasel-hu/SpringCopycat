baccf465f1e0311aee8963a663b096f764a01d80
chasel <1620646939@qq.com>
2017-03-08 14:36:38 +0800

    代表注解的类

---
+++ src/com/chasel/annotation/FieldInject.java
@@ -0,0 +1,14 @@
+package com.chasel.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface FieldInject {
+	//假设有一些变量用于控制策略
+}
